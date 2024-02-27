c80178fc6504077c1821e5c6d8f09e291909d11a
Rohit Badole <roohitbadole@gmail.com>
2024-02-27 11:46:58 +0530

    Added fibonacci code

---
+++ JavaPractice/src/practice_codes/Fibonacci.java
@@ -0,0 +1,28 @@
+package practice_codes;
+import java.util.Scanner;
+
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		
+		int n,a = 0,b = 1,c = 0;
+		System.out.println("Enter a last number to print fibonacci series - ");
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		System.out.print(a+" "+b+" ");
+		while (c<n)
+		{
+			if((c=a+b)>n)
+			{
+				break;
+			}
+			c=a+b;
+			System.out.print(c+" ");
+			a=b;
+			b=c;
+		}
+
+	}
+
+}
