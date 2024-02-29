817d518a8716f0589555c48dd1f0ac0f160a14aa
rohit <rohit@192.168.1.104>
2024-02-29 20:07:57 +0530

    added try catch in SortArray.java

--- JavaPractice/src/practice_codes/SortArray.java
+++ JavaPractice/src/practice_codes/SortArray.java
@@ -4,34 +4,35 @@ public class SortArray {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of the array you want- ");
-		int n = sc.nextInt();
-		int a[] = new int[n];
-		System.out.println("Enter the "+n+" elements for an array-");
-		for(int i=0;i<n;i++)
-		{
-			a[i]=sc.nextInt();
-		}
-		int temp ;
-		for(int i=0;i<a.length;i++)
-		{
-			for(int j=i+1;j<a.length;j++)
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter the size of the array you want- ");
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			System.out.println("Enter the "+n+" elements for an array-");
+			for(int i=0;i<n;i++)
 			{
-				if(a[i]>a[j])
-				{
-					temp=a[i];
-					a[i]=a[j];
-					a[j]=temp;		
-					
-				}
+				a[i]=sc.nextInt();
 			}
-			
-		}
-		System.out.println("Sorted array is");
-		for(int k=0;k<a.length;k++)
-		{
-			System.out.print(a[k]+" ");
+			int temp ;
+			for(int i=0;i<a.length;i++)
+			{
+				for(int j=i+1;j<a.length;j++)
+				{
+					if(a[i]>a[j])
+					{
+						temp=a[i];
+						a[i]=a[j];
+						a[j]=temp;		
+						
+					}
+				}
+				
+			}
+			System.out.println("Sorted array is");
+			for(int k=0;k<a.length;k++)
+			{
+				System.out.print(a[k]+" ");
+			}
 		}
 
 	}
