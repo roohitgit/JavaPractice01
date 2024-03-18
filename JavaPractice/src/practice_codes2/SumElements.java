b8698ee7dd5016b5a0f5701cbcad760fe13d3e70
rohit <rohit@192.168.1.104>
2024-03-18 21:21:29 +0530

    SumElements

---
+++ JavaPractice/src/practice_codes2/SumElements.java
@@ -0,0 +1,19 @@
+package practice_codes2;
+
+public class SumElements {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		int sum=0;
+		for(int a:arr)
+		{
+			sum=sum+a;
+		}
+		
+		
+		System.out.println(sum);
+		
+	}
+
+}
