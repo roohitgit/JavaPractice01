0c42d7d6157c6a6364afea067ae4b4de6e161113
rohit <roohitbadole@gmail.com>
2024-03-25 21:42:50 +0530

    LeapYearOrNot

--- JavaPractice/src/practice_codes2/LeapYearOrNot.java
+++ JavaPractice/src/practice_codes2/LeapYearOrNot.java
@@ -1,8 +1,21 @@
 package practice_codes2;
+import java.util.Scanner;
 
 public class LeapYearOrNot {
 
 	public static void main(String[] args) {
+		
+		
+		System.out.println("Enter a year- ");
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		
+		if(year%4==0)
+		{
+			System.out.println("This "+year+" year is a leap year");
+		}
+		else 
+			System.out.println("This "+year+" year is not a leap year");
 		
 	}
 
