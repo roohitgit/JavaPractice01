45a23e1176604655c0bfe1c5a6f33908ff4a82e2
Rohit Badole <roohitbadole@gmail.com>
2024-03-26 09:31:40 +0530

    Code In ElementsPresentInAtleaseTwoArrays

--- JavaPractice/src/practice_codes2/ElementsPresentInAtleaseTwoArrays.java
+++ JavaPractice/src/practice_codes2/ElementsPresentInAtleaseTwoArrays.java
@@ -1,11 +1,45 @@
 package practice_codes2;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class ElementsPresentInAtleaseTwoArrays {
 
 	public static void main(String[] args) {
+	
+		Integer a1[] = {1,2,3,9,8,7};
+		Integer a2[] = {4,1,2,10,15};
+		Integer a3[] = {5,1,2,4,10};
 		
+		HashSet<Integer> hs = new HashSet<>();
+		List l1 = Arrays.asList(a1);
+		List l2 = Arrays.asList(a2);
+		List l3 = Arrays.asList(a3);
+		hs.addAll(l1);
+		hs.addAll(l2);
+		hs.addAll(l3);
 		
-		//https://www.youtube.com/watch?v=HEUQLE7aULc&list=PLF9tovyahfL020hGgLIsRMZY4bfSLCFUa&index=57
+		List finalList = new ArrayList();
+		
+		for(int number : hs)
+		{
+			if(
+					(l1.contains(number) && l2.contains(number)) ||
+					(l2.contains(number) && l3.contains(number)) ||
+					(l1.contains(number) && l3.contains(number))
+					)
+			{
+				
+				finalList.add(number);
+			}
+			}
+
+			System.out.println(finalList);
+		
+		}
 	}
 
-}
+
