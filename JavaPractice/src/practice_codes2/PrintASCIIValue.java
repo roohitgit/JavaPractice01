8ddf2f658192fd2fba0305294011b398f4e09263
rohit <roohitbadole@gmail.com>
2024-03-29 20:15:16 +0530

    print ascii value of char

---
+++ JavaPractice/src/practice_codes2/PrintASCIIValue.java
@@ -0,0 +1,14 @@
+package practice_codes2;
+
+public class PrintASCIIValue {
+
+	public static void main(String[] args) {
+		
+		char ch = 'A';
+		int i=ch;
+		System.out.println("ASCII value of "+ch+" is "+i);
+		
+
+	}
+
+}
