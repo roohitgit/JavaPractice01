9f0b6f93d2e37080c00834b1b275dacfe5f70fb8
rohit <roohitbadole@gmail.com>
2024-03-29 20:34:47 +0530

    Print Alphabets a to z

---
+++ JavaPractice/src/practice_codes2/PrintAlphabets.java
@@ -0,0 +1,22 @@
+package practice_codes2;
+
+public class PrintAlphabets {
+
+	public static void main(String[] args) {
+		
+		for(char ch='a';ch<='z';ch++)
+		{
+			System.out.print(ch+" ");
+		}
+		
+		System.out.println();
+		
+		for(char ch='A';ch<='Z';ch++)
+		{
+			System.out.print(ch+" ");
+		}
+		
+
+	}
+
+}
